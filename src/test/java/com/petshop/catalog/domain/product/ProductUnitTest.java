ad33802299b2e1013e997c9d122d07debed8b4ce
Kevin Guevara <kevinguevara02@gmail.com>
2026-05-08 09:35:46 -0500

    Dockerizacion, correccion en la prueba unitaria de Dominio/Producto

--- src/test/java/com/petshop/catalog/domain/product/ProductUnitTest.java
+++ src/test/java/com/petshop/catalog/domain/product/ProductUnitTest.java
@@ -1,9 +1,11 @@
 package com.petshop.catalog.domain.product;
 
+import com.petshop.catalog.domain.DomainEvent;
 import com.petshop.catalog.domain.product.events.ProductCreated;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,10 +19,11 @@ class ProductUnitTest {
                 "descripcion",
                 BigDecimal.valueOf(100)
         );
+        List<DomainEvent> events = product.pullDomainEvents();
 
         assertNotNull(product);
         assertEquals("Producto Prueba", product.getName());
-        assertEquals(1, product.pullDomainEvents().size());
-        assertTrue(product.pullDomainEvents().get(0) instanceof ProductCreated);
+        assertEquals(1, events.size());
+        assertInstanceOf(ProductCreated.class, events.get(0));
     }
 }
